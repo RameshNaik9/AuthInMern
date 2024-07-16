e06279d0b4ab7c2fecdc50de25b71524153ba9d8
RameshNaik9 <lahorirameshnaik@gmail.com>
2024-07-16 14:49:58 +0530

    build serve files

--- server/index.js
+++ server/index.js
@@ -14,22 +14,22 @@ connection();
 app.use(express.json());
 app.use(cors());
 
-const buildPath = path.join(__dirname, '../client/build')
+// const buildPath = path.join(__dirname, '../client/build')
 
-app.use(express.static(buildPath))
+// app.use(express.static(buildPath))
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(buildPath, "index.html"));
+// });
+
+const buildPath = path.join(__dirname, 'build');
+
+app.use(express.static(buildPath));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
 
-// Serve static files from the React app
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
-// API routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
